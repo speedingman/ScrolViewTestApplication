2ef9e060c2894f4867d926101c2571aaeac9bf3e
speedingman <hcdo@andats.com>
2018-12-29 14:03:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/zeros/scrolviewtestapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.zeros.scrolviewtestapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupEvents();
+        setValues();
+        bindViews();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
